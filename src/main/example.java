43055ff85543e94fab6c3e658116fe769ae2dd15
Neeraja-B <130729267+Neeraja-B@users.noreply.github.com>
2023-05-15 07:11:46 +0000

    Create example.java

---
+++ src/main/example.java
@@ -0,0 +1,8 @@
+public class Example {
+public static void main(String[] args) {
+      Example object = new Example();
+       int a=5 , b=10 ;
+       int product = a*b;
+       System.out.println("The product of 10 and 5 is:"+product);
+   }
+}
