b82fce7722d2d1dec232cb9ee7fe26bdc2eeb2b8
Neeraja-B <130729267+Neeraja-B@users.noreply.github.com>
2023-05-17 03:47:20 +0000

    Update ExampleTest.java

--- src/test/ExampleTest.java
+++ src/test/ExampleTest.java
@@ -1,7 +1,10 @@
-import org.junit.Test;
-import static org.junit.Assert.*;
+ import org.junit.Test;
+ import static org.junit.Assert.*;
+ import org.junit.Rule;
+ import org.junit.jupiter.api.*;
 
-public class ExampleTest {
+
+ public class ExampleTest {
 
     @Test
     public void testProduct() {
@@ -11,4 +14,17 @@ public class ExampleTest {
         int actualProduct = example.getProduct(a, b);
         assertEquals(expectedProduct, actualProduct);
     }
+    
+ public static class WatchmanTest {
+  private static String watchedLog;
+
+  @Rule
+  public TestWatcher watchman= new TestWatcher() {
+      @Override
+      protected void failed(Throwable e, Description description) {
+          watchedLog+= description + "\n";
+      }
+  @Test
+  public void fails() {
+      fail();
 }
