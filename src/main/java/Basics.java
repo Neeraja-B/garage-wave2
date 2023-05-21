fa6e79efb45cd4f812582e4f840ef3f3f9feeddb
Neeraja-B <130729267+Neeraja-B@users.noreply.github.com>
2023-05-21 23:39:22 +0530

    Update Basics.java

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -5,15 +5,4 @@
         return 0;
     }
 
-    public int max(int[] arr) {
-        if(arr.length == 0) return -1;
-
-        int max = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] > max) {
-                max = arr[i];
-            }
-        }
-        return max;
-    }
-  }
+    
