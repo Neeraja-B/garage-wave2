544bfda06333829b3a7e24b59679ae7ea2361ebf
Neeraja-B <130729267+Neeraja-B@users.noreply.github.com>
2023-05-21 23:27:47 +0530

    Update and rename TestDemo.java to TestDemoClass.java

--- src/test/java/TestDemoClass.java
+++ src/test/java/TestDemoClass.java
@@ -1,7 +1,7 @@
 import static org.junit.Assert.*;  
 import org.junit.Test;
-public class TestDemo {  
-DemoClass obj=new DemoClass();   
+public class TestDemoClass {  
+TestDemoClass obj=new TestDemoClass();   
 @Test  
 public void testSum() {  
     assertEquals(25,obj.sum(10, 15));  
