8c9a6bd8e734a30931614007fbbb24e15b03c10e
Neeraja-B <130729267+Neeraja-B@users.noreply.github.com>
2023-05-22 16:46:06 +0530

    Update and rename CalculatorTest.java to ExampleTest.java

---
+++ src/test/java/ExampleTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Rule;
+
+public class ExampleTest {
+
+  @Test
+  public void testProduct() {
+    Example example = new Example();
+    int a = 5, b = 10;
+    int expectedProduct = 50;
+    int actualProduct = example.getProduct(a, b);
+    assertEquals(expectedProduct, actualProduct);
+  }
+
+}
