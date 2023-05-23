0c28f104d8b9ad05aefb4e4660596ca75e9b70ce
Neeraja-B <130729267+Neeraja-B@users.noreply.github.com>
2023-05-23 17:54:55 +0530

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,7 +8,7 @@ public class App {
 }
  private String reverse(String input) {
   String rev = "";
-  for (int i = input.length() 1; i>=0; i--) {  
+  for (int i = input.length()-1; i>=0; i--) {  
      rev = rev + input.charAt(i);
   }
   return rev;
