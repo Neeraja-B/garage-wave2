212b4ee247a7c390c2a8d505f821cab6ffff5dbb
Neeraja-B <130729267+Neeraja-B@users.noreply.github.com>
2023-05-23 12:21:43 +0000

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,13 +1,16 @@
 public class App {
- public boolean is Palindrome (String input) {
-  if (input.equals (reverse (input))) { return true;
+ public boolean isPalindrome (String input) {
+  if (input.equals(reverse(input))) {
+   return true;
    } else {
     return false;
    }
 }
  private String reverse(String input) {
- String rev = "";
- for (int i = input.length() 1; i = 0; i--) { }
-  rev = rev + input.charAt(i);
+  String rev = "";
+  for (int i = input.length() 1; i>=0; i--) {  
+     rev = rev + input.charAt(i);
+  }
   return rev;
+}
 }
