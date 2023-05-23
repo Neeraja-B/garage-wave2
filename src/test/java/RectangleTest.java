d5628f3303dd4a05e0ea211296bc65410959b5fb
Neeraja-B <130729267+Neeraja-B@users.noreply.github.com>
2023-05-23 09:23:24 +0530

    Update RectangleTest.java

--- src/test/java/RectangleTest.java
+++ src/test/java/RectangleTest.java
@@ -1,14 +1,12 @@
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 import org.junit.Test;
 
-public class RectangleTest {
-
+public class TestDemoClass {
+    DemoClass obj = new DemoClass();
     @Test
-    public void testGetArea() {
-        Rectangle rectangle = new Rectangle(5, 10);
-        int expected = 50;
-        int result = rectangle.getArea();
-        assertEquals(expected, result);
+    public void testsum() {
+       
+        assertEquals(25,obj.sum(10,15));
     }
 }
