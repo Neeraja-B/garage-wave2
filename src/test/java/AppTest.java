bea255e35bf8b56ad70744316aef67d1021161a4
Neeraja-B <130729267+Neeraja-B@users.noreply.github.com>
2023-05-23 11:59:20 +0000

    Update and rename RectangleTest.java to AppTest.java

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,18 @@
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class TestDemoClass {
+    
+    String input1= "noon";
+    App app = new App();
+    boolean expected=true;
+    
+    @Test
+    public void isPalindromeTest() {
+        assertEquals(expected, app.isPalindrome(input1));       
+     @Test
+    public void isNotPalindromeTest() {
+        assertEquals(false, app.isPalindrome(abc));       
+    }
+}
